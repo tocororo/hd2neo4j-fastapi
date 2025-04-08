d62f4fd0b8acb475b9661df449a1776c4f9b2ff9
Jorge Luis Arencibia <jorge9815rdz@gmail.com>
2025-04-08 19:48:18 -0400

    refactor: removed routes file

--- hd2neo4j_fastapi/main.py
+++ hd2neo4j_fastapi/main.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File
 import logging
 import uvicorn
+from typing import Annotated
+from hd2neo4j_fastapi.service_register import ServiceRegistry
 
-from hd2neo4j_fastapi.routes import mapper_routes
 
 logging.basicConfig(filename="logfile.log", level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -11,7 +12,30 @@ logger = logging.getLogger(__name__)
 app = FastAPI(title="hd2neo4j standalone tool", version="0.9.0")
 
 
-app.include_router(mapper_routes.rt)
+@app.get("/")
+def get_graph(): 
+    response = ServiceRegistry().get_repository_service().get_graph()
+    return response.records
+
+
+@app.get("/query")
+def execute_query(query: str):
+    return ServiceRegistry().get_repository_service().execute_external_query(query)
+
+
+@app.post("/start")
+def start_mapping(
+    configFile: Annotated[bytes, File()],
+    dataFile: Annotated[bytes, File()],
+):
+    controller = ServiceRegistry().get_mapper_service(mapping_config=configFile, data_to_map=dataFile)
+    controller.start_mapping()
+
+
+@app.delete("")
+def drop_db():
+    ServiceRegistry().get_repository_service().clean_graph_db()
+
 
 
 def start():
